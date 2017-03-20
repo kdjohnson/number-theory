00d35f5ef2c244e74b7e05362b39e6ff96161d05
KaJuan Johnson <kdjohnson23@oakland.edu>
2017-03-20 14:45:08 -0400

    Add Quadratic Residues and Non-Quadratic Residues

---
+++ quadraticResidues.hs
@@ -0,0 +1,23 @@
+import Data.List (nub, elemIndex)
+import Data.Ratio
+import Data.Maybe
+import Math.NumberTheory.Primes.Factorisation
+
+main = print "ready"
+
+gcd' :: (Integral a) => a -> a -> a   
+gcd' a 0 = a
+gcd' a b = gcd b a `rem` b
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) =
+  let smallerSorted = quickSort [a | a <- xs, a <= x]
+      biggerSorted = quickSort [a | a <- xs, a > x]
+  in  smallerSorted ++ [x] ++ biggerSorted
+
+quadraticRoots :: (Integral a) => a -> [a]
+quadraticRoots m = quickSort (nub [ x ^ 2 `mod` m | x <- [1..(m - 1)]]) 
+
+nonQuadraticResidues :: (Integral a) => a -> [a] -> [a]
+nonQuadraticResidues m xs = [ x | x <- [1..(m-1)], x `notElem` xs] 
